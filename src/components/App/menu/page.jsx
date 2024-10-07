74c45f3535f682018810af5b78efe03968dd1c46
Erfanfooladii <arfannnfooladi@gmail.com>
2024-10-07 10:14:24 +0330

    feat: Add a component for menu

---
+++ src/components/App/menu/page.jsx
@@ -0,0 +1,16 @@
+const Menu=()=>{
+    return(
+        <>
+            <div className="menu-full">
+                <ul>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </div>
+        </>
+    )
+}
